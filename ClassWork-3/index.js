093bf707fc3c8e1652855d905c2dbe4380e82176
rima1215akter <rima92373@gmail.com>
2021-11-20 00:28:19 +0600

    added-tags

---
+++ ClassWork-3/index.js
@@ -0,0 +1,5 @@
+function updateJavascript(){
+    let x=document.getElementById('myImage');x.src="img/picm1.jpg";
+    let y=document.getElementById('myImage1');y.src="img/pic2.jpg";
+    let z=document.getElementById('myImage2');z.src="img/pic3.jpg";
+}
